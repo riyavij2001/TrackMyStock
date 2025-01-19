e84b0d503c749a82e34e12b8b75425ad1aadb34b
Riya <riya.vij@cyraacs.com>
2025-01-19 20:05:53 +0530

    Added common components

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { HeroUIProvider } from '@heroui/react'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
+  </StrictMode>,
+)
